5be832d8516f0017d0985f4b7b0a384c30363025
prince kumar bhaskar <163023151+Princebhaskar@users.noreply.github.com>
2026-08-03 22:17:33 +0530

    Time: 11 ms (28.30%) | Memory: 11.5 MB (37.30%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+    int memo[501][501];
+    int solve(int i, int j, vector<int>&piles){
+        if(i>j){
+            return 0;
+        }
+        if(memo[i][j] != -1)return memo[i][j];
+        int takeLeft = piles[i] - solve(i+1, j, piles);
+        int takeRight = piles[j]-solve(i,j-1,piles);
+
+        return memo[i][j] = max(takeLeft, takeRight);
+        //return max(takeLeft, takeRight);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n =piles.size();
+        memset(memo, -1, sizeof(memo));
+        return solve(0,n-1,piles)>0;
     }
 };
