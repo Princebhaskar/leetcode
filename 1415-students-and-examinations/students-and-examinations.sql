2ba15070c74a52242ad89c158153bb3166d61e9d
prince kumar bhaskar <163023151+Princebhaskar@users.noreply.github.com>
2026-08-09 18:58:04 +0530

    Time: 1388 ms (11.41%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -4,5 +4,5 @@ FROM Students s CROSS JOIN Subjects sb
 LEFT JOIN Examinations e
 ON e.student_id = s.student_id
 AND e.subject_name = sb.subject_name
-GROUP BY s.student_id, s.student_name, sb.subject_name
+GROUP BY s.student_id, sb.subject_name
 ORDER BY student_id ASC ;
