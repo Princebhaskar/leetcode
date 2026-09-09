f0f424619bcc6bca22551f359bc99f86c4e55aea
prince kumar bhaskar <163023151+Princebhaskar@users.noreply.github.com>
2026-09-09 18:54:25 +0530

    Time: 2 ms (4.01%) | Memory: 8.5 MB (56.31%) - LeetSync

--- 1369-minimum-swaps-to-make-strings-equal/minimum-swaps-to-make-strings-equal.cpp
+++ 1369-minimum-swaps-to-make-strings-equal/minimum-swaps-to-make-strings-equal.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
     int minimumSwap(string s1, string s2) {
-        vector<int>f(26,0);
-        for(char c:s1)f[c-'a']++;
-        for(char c:s2)f[c-'a']++;
-        for(int i=0;i<26;i++)if(f[i]&1)return -1;
-        int res = 0 ;
-        int n=s1.size();
-        
-        // vector<int>f1(2, 0) , f2(2, 0) ;
-        for(int i=0;i<n;i++){
-            if(s1[i]==s2[i])continue;
-            for(int j=i+1;j<n;j++){
-                if(s1[j]==s2[j])continue;
-                if(s1[i]==s1[j]){
-                    res++;
-                    swap(s1[j], s2[i]);
-                    break;
-                }
-            }
-            if(s1[i]==s2[i])continue;
-            for(int j=i+1;j<n;j++){
-                if(s1[j]==s2[j]) continue;
-                if(s1[i]==s2[j]){
-                    res+=2;
-                    swap(s2[j], s2[i]);
-                    break;
-                }
-            }
+        int xy=0;
+        int yx=0;
+        int n = s1.size() ;
+        for(int i=0; i<n; i++) {
+            if(s1[i] == s2[i]) continue ;
+            if(s1[i]=='x' && s2[i]== 'y' ) xy++;
+            else yx++; 
         }
 
-        return res ;
+        if((xy+yx) % 2 != 0) return -1;
+        return xy/2 + yx / 2 + (xy % 2) * 2;
+
     }
 };
